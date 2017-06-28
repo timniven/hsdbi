bacb76a556548de0cfa8fd1c09ce90cc1f908ae5
Tim Niven <tim.niven.public@outlook.com>
2017-06-28 20:52:19 +0800

    Added license and cfg

--- setup.py
+++ setup.py
@@ -7,18 +7,19 @@ setup(
     long_description='Exposes a common interface for all kinds of database '
                      'connections. In version 0.1, SQL databases (via '
                      'sqlalchemy) and MongoDB are implemented.',
-    version='0.0.1',
+    version='0.1a',
     url='https://github.com/timniven/hsdbi',
+    download_url='https://github.com/timniven/hsdbi/archive/0.1a.tar.gz',
     author='Tim Niven',
     author_email='tim.niven.public@gmail.com',
-    license='Apache2',
+    license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
-        'License :: OSI Approced :: Apache Free License (AFL)'
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
     ],
-    packages=[],
+    packages=['hsdbi'],
     install_requires=[
         'pymongo>=3.4.0',
         'sqlalchemy>=1.1.11'
