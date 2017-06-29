e9d6ef61e4d4f8d097d10c51b10d234b329ddc17
Tim Niven <tim.niven.public@outlook.com>
2017-06-29 09:46:55 +0800

    Cleaned up mongo.py documentation.

--- hsdbi/mongo.py
+++ hsdbi/mongo.py
@@ -1,3 +1,4 @@
+"""MongoDB implementations."""
 from hsdbi import base
 import pymongo
 from hsdbi import errors
@@ -7,6 +8,14 @@ from hsdbi import errors
 
 
 def projection_dict(projection):
+    """Get a projection dictionary from a list of attribute names to project.
+
+    Args:
+      projection: List of string names of attributes to project.
+
+    Returns:
+      Dictionary like {'attr1': 1, 'attr': 1, ... }.
+    """
     if projection:
         return dict(zip(projection, [1] * len(projection)))
     else:
@@ -19,51 +28,67 @@ def projection_dict(projection):
 class MongoRepositoryFacade(base.RepositoryFacade):
     """Repository Facade implementation for MongoDB.
 
-    With an SQL Repository facade, we connection to a single database instance.
-    With a Mongo Repository facade, we connect to the database server. So we
-    will not specify a database to connect to. We leave this to the Repository
-    class. This will create flexibility in the number of connections we
-    potentially need to make.
+    With an SQL Repository facade, we connecti to a single database instance.
+    With a Mongo Repository facade however, we connect to the database server.
+    So we will not specify a database to connect to. We leave this to the
+    Repository class. This will create flexibility in the number of connections
+    we potentially need to make.
+
+    The upshot of this thinking is: pass a pymongo.mongo_client.MongoClient to
+    the Repository classes, instead of a connection to a specific collection.
+    We also make this connection public on the class for convenience for
+    consumers and extensibility.
+
+    Attributes:
+      connection: pymongo.mongo_client.MongoClient, connection to the database
+        server.
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, server, port, **kwargs):
         """Create a new MongoRepositoryFacade.
 
-        The following keyword arguments are expected:
-          server
-          port
-          db_name
+        Args:
+          server: String, the address of the server. E.g. 'localhost'.
+          port: Integer, the port number to connect to. E.g. 27017.
         """
         super(MongoRepositoryFacade, self).__init__(**kwargs)
-        self._server = kwargs['server']
-        self._port = kwargs['port']
-        self._db_name = kwargs['db_name']
-        self._connection = pymongo.MongoClient(
+        self._server = server
+        self._port = port
+        self.connection = pymongo.MongoClient(
             host=self._server, port=self._port)
 
+    def __enter__(self):
+        self.__init__(self._server, self._port, **self._kwargs)
+        return self
+
     def dispose(self):
-        self._connection.close()
+        self.connection.close()
 
 
 class MongoRepository(base.Repository):
     """Repository implementation for MongoDB."""
 
-    def __init__(self, **kwargs):
+    def __init__(self, connection, db_name, collection_name, **kwargs):
         """Create a new MongoRepository.
 
-        Args (expected keyword args):
-          collection_name: String, the name of the collection for this
-            Repository to access.
-          db_name: the name of the db for this Repository to connect to.
+        Args:
           connection: pymongo.mongo_client.MongoClient, connection to the
             database server.
+          db_name: String, the name of the database we intend to connect to.
+          collection_name: String, the name of the collection we intend to
+            connect to.
         """
         super(MongoRepository, self).__init__(**kwargs)
-        self._connection = kwargs['connection']
-        self._collection_name = kwargs['collection_name']
-        self._db_name = kwargs['db_name']
+        self._connection = connection
+        self._collection_name = collection_name
+        self._db_name = db_name
         self._db = self._connection.get_database(self._db_name)
         self._collection = self._db.get_collection(self._collection_name)
+
+    def __enter__(self):
+        self.__init__(self._connection, self._db_name, self._collection_name,
+                      **self._kwargs)
+        return self
 
     def add(self, items=None, **kwargs):
         """Add one or more items to the database.
@@ -100,11 +125,15 @@ class MongoRepository(base.Repository):
         return self._collection.find({}, projection_dict(projection))
 
     def commit(self):
-        """Override is forced, so we ensure what logic we want."""
+        """Does nothing for a MongoRepository."""
         pass
 
     def count(self):
-        """Count the number of records in the collection."""
+        """Count the number of records in the collection.
+
+        Returns:
+          Integer, the number of records in the collection.
+        """
         return self._collection.count()
 
     def delete(self, items=None, **kwargs):
@@ -117,7 +146,8 @@ class MongoRepository(base.Repository):
         Args:
           items: one or more objects of the intended type; can be a list or
             a single object.
-          kwargs: is a document to select.
+          kwargs: specify the attribute name(s) and value(s) of a document to
+            be deleted.
 
         Raises:
           ValueError: if neither items nor keyword arguments are specified.
@@ -136,11 +166,11 @@ class MongoRepository(base.Repository):
             self._collection.delete_one(kwargs)
 
     def delete_all_records(self):
-        """Delete all records from this table."""
+        """Delete all records from this collection."""
         self._collection.drop()
 
     def dispose(self):
-        """Dipose of the MongoRepository."""
+        """Dispose of the database server connection."""
         self._connection.close()
 
     def get(self, expect=True, projection=None, **kwargs):
@@ -169,12 +199,14 @@ class MongoRepository(base.Repository):
         return item
 
     def search(self, projection=None, **kwargs):
-        """Attempt to get items (plural) from the database.
+        """Attempt to get item(s) from the database.
 
-        Pass whatever attributes you want to keyword arguments.
+        Pass whatever attributes you want as keyword arguments.
 
         Args:
           projection: List of String attribute names to project, optional.
+          kwargs: the attribute name(s) and value(s) to be used for search.
+            If empty this function is equivalent to all().
 
         Returns:
           pymongo.cursor.Cursor with matching results (if any).
